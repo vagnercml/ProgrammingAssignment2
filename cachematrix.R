28a6394607712fc71c62510d05cb5524d599206b
vagnercml <vagnercml@hotmail.com>
2017-09-29 17:57:18 -0300

    Test of RStudio with GitHub

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  print("makeCacheMatrix")
 }
 
 
@@ -12,4 +12,5 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  print("cacheSolve")
 }
